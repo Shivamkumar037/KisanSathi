ea39488e5e632e1bca2010b622c86fbe508507d1
Shivam Kumar <shivamkumar37je@gmail.com>
2026-04-14 01:06:55 -0700

    Delete app directory

--- app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.models.user import User
-from app.models.crop import CropScan
-from app.models.market import MarketRate
-from app.models.scheme import GovernmentScheme
-from app.models.community import CommunityPost, Comment, PostLike
-from app.models.chat import ChatSession, ChatMessage
